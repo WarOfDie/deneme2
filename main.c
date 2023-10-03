ead95c0bf415ea0f33dd988e1932edf56987a5f2
WarOfDie <94315325+WarOfDie@users.noreply.github.com>
2023-10-03 21:47:09 +0300

    new message added

--- main.c
+++ main.c
@@ -15,7 +15,7 @@
 #include "driverlib/adc.h"
 #include "lcd.h"
 #include "driverlib/uart.c"
-//yeni commit mesajý usman nere
+//yeni commit mesajý usman nere diff denemesdfgsdfgsdfgsdfgsdf
 uint32_t gelendeger[4];
 uint32_t adcveri[4];
 uint32_t gelenveri[8];
